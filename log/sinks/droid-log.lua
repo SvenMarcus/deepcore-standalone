f57ed759986bc530a8b2b2d786c485ecbb33a73f
Sven Marcus <s.marcus@outlook.de>
2021-10-22 23:50:02 +0200

    Remove the wrong log folder

--- log/sinks/droid-log.lua
+++
@@ -1,9 +0,0 @@
-return {
-    __important = true,
-    __id = "droid-log",
-    __has_character_limit = true,
-    __log = function(self, message)
-        local msg = tostring(message)
-        Game_Message(msg)
-    end
-}
